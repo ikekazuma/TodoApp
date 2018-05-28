8fa81f6143f44f9dfea4ebd50ad021397f8667ac
kazuma_ike <kazuma_ike@yaz.co.jp>
2018-05-29 00:51:16 +0900

    refactor api to use request body

--- app/src/main/java/com/example/ike/todoapp/data/api/TodoAPI.java
+++ app/src/main/java/com/example/ike/todoapp/data/api/TodoAPI.java
@@ -1,26 +1,28 @@
 package com.example.ike.todoapp.data.api;
 
+import com.example.ike.todoapp.data.api.request.AddTodosRequest;
+import com.example.ike.todoapp.data.api.request.DeleteTodosRequest;
+import com.example.ike.todoapp.data.api.request.GetTodosRequest;
 import com.example.ike.todoapp.data.api.response.GetTodosResponse;
 import com.example.ike.todoapp.data.api.response.UserResponse;
 
 import io.reactivex.Flowable;
 import retrofit2.Response;
-import retrofit2.http.DELETE;
-import retrofit2.http.GET;
+import retrofit2.http.Body;
+import retrofit2.http.HTTP;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface TodoAPI {
 
     @POST("user")
     Flowable<UserResponse> postUser();
 
-    @GET("todos")
-    Flowable<GetTodosResponse> getTodos(@Query("token") String token);
+    @POST("todos/all")
+    Flowable<GetTodosResponse> getTodos(@Body GetTodosRequest request);
 
     @POST("todos")
-    Flowable<Response<Void>> addTodos(@Query("token") String token, @Query("date") String date, @Query(value = "title", encoded = true) String title, @Query(value = "content", encoded = true) String content);
+    Flowable<Response<Void>> addTodos(@Body AddTodosRequest request);
 
-    @DELETE("todos")
-    Flowable<Response<Void>> deleteTodos(@Query("token") String token, @Query("date") String date);
+    @HTTP(method = "DELETE", path = "todos", hasBody = true)
+    Flowable<Response<Void>> deleteTodos(@Body DeleteTodosRequest request);
 }
---
+++ app/src/main/java/com/example/ike/todoapp/data/api/request/GetTodosRequest.java
@@ -0,0 +1,8 @@
+package com.example.ike.todoapp.data.api.request;
+
+public class GetTodosRequest {
+    public String token;
+    public GetTodosRequest(String token) {
+        this.token = token;
+    }
+}
